b87c6f00c1a7cc8bfb6c9be0cc359f7ba545006f
Matt Moore <Matt.M.Moore13@outlook.com>
2021-03-26 17:00:14 -0400

    Attempting to deploy and speak to the api

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://frozen-thicket-25455.herokuapp.com/',
+  production: 'https://frozen-thicket-25455.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
