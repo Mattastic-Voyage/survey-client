eff5e1744b423a74d4be676f22589ff4ef1f5efb
Matt Moore <Matt.M.Moore13@outlook.com>
2021-03-28 10:27:05 -0400

    Changed the title in the navbar

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -28,7 +28,7 @@ const alwaysOptions = (
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      react-auth-template
+      SurveyUs
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
