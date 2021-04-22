6ca68edd70286ae1a200bb0f71e07d94615ea791
mjeder <matteder96@gmail.com>
2021-04-22 01:40:39 -0500

    Update font and theme

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,18 @@
 import React, { Fragment } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 const authenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#change-password">Change Password</Nav.Link>
-    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
-    <Nav.Link href="#create-survey">Make Survey</Nav.Link>
-    <Nav.Link href="#surveys">View All Surveys</Nav.Link>
+    <NavDropdown title="Surveys" id="basic-nav-dropdown" className="mr-3">
+      <NavDropdown.Item href="#create-survey">New Survey</NavDropdown.Item>
+      <NavDropdown.Item href="#surveys">View Surveys</NavDropdown.Item>
+    </NavDropdown>
+    <NavDropdown title="Account" id="basic-nav-dropdown" className="mr-3">
+      <NavDropdown.Item href="#change-password">Change Password</NavDropdown.Item>
+      <NavDropdown.Item href="#sign-out">Sign Out</NavDropdown.Item>
+    </NavDropdown>
   </Fragment>
 )
 
@@ -21,19 +26,18 @@ const unauthenticatedOptions = (
 
 const alwaysOptions = (
   <Fragment>
-    <Nav.Link href="#/">Home</Nav.Link>
+    <Nav.Link href="#/" className="mr-3">Home</Nav.Link>
   </Fragment>
 )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
+  <Navbar className="nav-bar" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      SurveyUs
+      <img src='logo-su-white.png'/>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
         { alwaysOptions }
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
